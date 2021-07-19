4e6a9fa96c0e29e41783baedefff84fead6b8077
Lilibeth Tabares <1762@holbertonschool.com>
2021-07-19 15:19:45 -0500

    pycodestyle fixed

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float],float]:
-    def mult (num: float):
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def mult(num: float):
         return num * multiplier
     return mult
