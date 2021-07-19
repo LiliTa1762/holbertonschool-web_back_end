8ec302db3bb0c0cd3768dd948e0279c28018ad40
Lilibeth Tabares <1762@holbertonschool.com>
2021-07-19 17:54:54 -0500

    Adding code

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 
 from typing import Iterable, List, Tuple, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """values with the appropriate types"""
     return [(i, len(i)) for i in lst]
