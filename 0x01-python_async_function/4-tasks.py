4b065b8ec501b98177ba868dd0ffd365ab5bec38
Lilibeth Tabares <1762@holbertonschool.com>
2021-07-20 17:35:42 -0500

    Mixing codes

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""wait_n and task_wait_random"""
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    delay_list: List()[float] = []
+    total_delays: List[float] = []
+
+    for i in range(n):
+        delay_list.append(task_wait_random(max_delay))
+
+    for delay_list in asyncio.as_completed(delay_list):
+        order = await delay_list
+        total_delays.append(order)
+
+    return total_delays
