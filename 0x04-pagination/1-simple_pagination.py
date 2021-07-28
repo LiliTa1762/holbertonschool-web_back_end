6fcbe0d73c900d619d4e0f84bcb1a170be7fe635
Lilibeth Tabares <1762@holbertonschool.com>
2021-07-28 10:05:04 -0500

    get_page method

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size), page * page_size
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page and number of item taking into account the page_size
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        list_dataset = []
+        list_dataset = self.dataset()
+        if page > len(self.dataset()):
+            return []
+        return list_dataset[page], list_dataset[page_size]
