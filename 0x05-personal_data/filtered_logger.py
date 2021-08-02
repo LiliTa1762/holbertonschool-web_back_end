5f38f4d21fbf3ed4d330f70389391fe882b24c7a
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-02 09:57:23 -0500

    log message obfuscated

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
     for f in fields:
