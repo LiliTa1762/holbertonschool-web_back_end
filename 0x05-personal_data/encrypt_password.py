ef80886199515173264d3aa178638d0c87399929
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-02 15:42:01 -0500

    Check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,3 +12,15 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     to_hash = bcrypt.hashpw(p, salt)
     return to_hash
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check valid password"""
+    p = b'password'
+
+    salt = bcrypt.gensalt()
+    to_hash = bcrypt.hashpw(p, salt)
+
+    if bcrypt.checkpw(p, to_hash):
+        return True
+    else:
+        return to_hash
