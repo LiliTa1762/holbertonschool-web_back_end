f1fb1d6f3d10fbb5bde0b15f53b85ee9f334580e
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-02 20:35:39 -0500

    Create logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,15 +9,7 @@ from typing import List
 import os
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """returns the log message obfuscated"""
-    for f in fields:
-        message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
-    return message
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -33,6 +25,7 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
+
     def format(self, record: logging.LogRecord) -> str:
         """filter values in incoming log records"""
         message_log = logging.Formatter(self.FORMAT).format(record)
@@ -40,6 +33,29 @@ class RedactingFormatter(logging.Formatter):
                                    message_log, self.SEPARATOR)
         return log_records
 
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """connect to secure database"""
