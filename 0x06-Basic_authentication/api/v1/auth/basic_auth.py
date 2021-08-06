fb72069c69941541bb48f77fea1cd2aead8e87b3
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-06 15:54:41 -0500

    Boom

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,3 +77,8 @@ class BasicAuth(Auth):
         """oerloads Auth and retrieves the User instance"""
         if not request:
             return None
+        auth_header = Auth().authorization_header(request)
+        auth_header = self.extract_base64_authorization_header(auth_header)
+        dec_header = self.decode_base64_authorization_header(auth_header)
+        cred = self.extract_user_credentials(dec_header)
+        return self.user_object_from_credentials(cred[0], cred[1])
