b1373a1122a250a6e517b3dda7709e89900c86a4
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-13 11:21:56 -0500

    User model

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -11,10 +11,10 @@ Base = declarative_base()
 
 
 class User(Base):
-	"""Class users"""
-	__tablename__ = 'users'
-	id = Column(Integer, primary_key=True)
-	email = Column(String(250), nullable=False)
-	hashed_password = Column(String(250), nullable=False)
-	session_id = Column(String(250), nullable=True)
-	reset_token = Column(String(250), nullable=True)
+    """Class users"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
