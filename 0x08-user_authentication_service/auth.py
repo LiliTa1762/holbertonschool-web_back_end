a6bb6207bd1496d98fb179754ff34d89079145ba
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-17 18:31:24 -0500

    Register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ class Auth:
         """init method"""
         self._db = DB()
 
-    def register_user(self, email: str, password: str)-> User:
+    def register_user(self, email: str, password: str) -> User:
         """to register a user, checking if email already exists"""
         try:
             user = self._db.find_user_by(email=email)
@@ -24,10 +24,9 @@ class Auth:
             password_hashed = _hash_password(password)
             user = self._db.add_user(email, password_hashed)
             return user
-        if user is None:
-            raise NoResultFound
         else:
             raise ValueError('User {email} already exists')
+
 
 def _hash_password(password: str) -> bytes:
     """Method to hash password"""
