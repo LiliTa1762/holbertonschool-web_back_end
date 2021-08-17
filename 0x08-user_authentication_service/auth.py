8b84c2e1da0f4f804b9eeff82606eaea4b489901
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-17 18:30:12 -0500

    Register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,7 +3,31 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """init method"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str)-> User:
+        """to register a user, checking if email already exists"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            password_hashed = _hash_password(password)
+            user = self._db.add_user(email, password_hashed)
+            return user
+        if user is None:
+            raise NoResultFound
+        else:
+            raise ValueError('User {email} already exists')
 
 def _hash_password(password: str) -> bytes:
     """Method to hash password"""
