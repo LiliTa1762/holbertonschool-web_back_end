0093ec2134bf28f1d45d6d839611c53484184307
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-17 12:01:23 -0500

    updating init

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Database module for ORM
+"""DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
@@ -12,11 +12,10 @@ from user import Base, User
 
 class DB:
     """DB class"""
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
