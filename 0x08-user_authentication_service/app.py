4576e59c05b139d06779c5ec3d85561eda3a375e
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-17 18:47:17 -0500

    flask app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello():
     """first route"""
-    return jsonify('{"message": "Bienvenue"}')
+    return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
