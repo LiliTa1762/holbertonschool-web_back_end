e3ad5aa7ebdc9ba3094493d0bd3a2d30952f6e80
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-17 11:37:54 -0500

    updating columns

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -2,8 +2,7 @@
 """SQLAlchemy model named User for a database table named users"""
 
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -16,5 +15,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
