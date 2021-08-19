4a0dd2edeeabc53ac2794bb9785030f2fdc687d8
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-18 21:16:45 -0500

    destroy

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -60,9 +60,7 @@ class Auth:
     def destroy_session(user_id: int) -> None:
         """Destroy session"""
         try:
-            user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user.id, session_id=None)
-            return None
+            self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
 
