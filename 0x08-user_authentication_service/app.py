96396717f1cd1c48ea271fff9313777f40bb49b0
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-18 21:46:22 -0500

    Log out

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -48,7 +48,7 @@ def logout():
     """Log out function"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    if session_id is None or user is None:
         abort(403)
     else:
         AUTH.destroy_session(user_id)
