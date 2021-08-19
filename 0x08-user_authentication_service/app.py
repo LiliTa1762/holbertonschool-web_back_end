2430340f04541d0d28e4f28137fa86f56bac2b3b
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-18 21:45:30 -0500

    Log out

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic Flask app"""
 
 
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
@@ -41,6 +41,18 @@ def login():
         resp = make_response(jsonify({"email": email, "message": "logged in"}))
         resp.set_cookie("session", session_id)
         return resp
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Log out function"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user_id)
+        return redirect('/')
 
 
 if __name__ == "__main__":
