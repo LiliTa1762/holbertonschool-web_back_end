953659de8107fcfea9fcafbc9c7e7d7936cd0fdb
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-18 23:19:45 -0500

    token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -64,6 +64,15 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user_id, reset_token=token)
+        return token
 
 def _hash_password(password: str) -> bytes:
     """Method to hash password"""
