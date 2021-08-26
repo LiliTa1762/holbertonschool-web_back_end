3d3abc4b534c5b3371dd40b86cf126ef23c2ffc6
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-26 15:48:39 -0500

    Get locale from request

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -19,10 +19,12 @@ class Config:
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
     """Get local from request"""
     return request.accept_languages.best_match(app.config['LANGUAGE'])
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
