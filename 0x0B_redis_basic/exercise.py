63fbffe8062560feffe1fde3ce5706d699156e0e
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-31 15:56:01 -0500

    adding Union

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,6 +5,7 @@
 from redis.client import Redis
 import redis
 import uuid
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -14,8 +15,22 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: [str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument, returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> str:
+        """convert the data back to the desired format"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self) -> str:
+        """conversion to get str"""
+
+
+    def get_int(self) -> int:
+        """conversion to get int"""
