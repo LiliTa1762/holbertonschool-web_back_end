a6ce4dbc42d2ee29cfc05a0d8101bec4425c103d
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-07 17:03:53 -0500

    wow logs

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     print(all, "logs")
 
     print("Methods:")
-    print("method GET:", log.count_documents({"method": "GET"}))
-    print("method POST:", log.count_documents({"method": "POST"}))
-    print("method PUT:", log.count_documents({"method": "PUT"}))
-    print("method PATCH:", log.count_documents({"method": "PATCH"}))
-    print("method DELETE:", log.count_documents({"method": "DELETE"}))
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = log.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
 
-    print(log.count_documents({"path": "/status"}), "status check")
+    check_get = log.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
