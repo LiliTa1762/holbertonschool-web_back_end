9c61110bbaeec42e500118f8d910d4209c466b32
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-07 15:44:39 -0500

    list with a topic

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -2,5 +2,5 @@
 """ Return the list of collection having a topic"""
 
 def schools_by_topic(mongo_collection, topic):
-	"""Return list of school with a topic"""
-	return mongo_collection.find({"topic": topic})
+    """Return list of school with a topic"""
+    return mongo_collection.find({ "topic": topic })
