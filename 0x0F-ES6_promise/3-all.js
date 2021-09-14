a9f8f3033d8723e3957e2036c2f847774e2b92f4
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-14 17:15:40 -0500

    all promises

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const resolvedPromisesArray = [uploadPhoto(), createUser()];
+  return Promise.all(resolvedPromisesArray)
+    .then((val) => console.log(val[0].body, val[1].firstName, val[1].lastName))
+    .catch(() => Error(console.log('Signup system offline')));
+}
