9e99f249e937aa4e266b5e1b5be213ea1e3c0465
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-29 16:46:39 -0500

    updating conditions

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,16 +1,22 @@
 function calculateNumber(type, a, b) {
-  roundA = Math.round(a);
-  roundB = Math.round(b);
-
   if (type === 'SUM') {
+    roundA = Math.round(a);
+    roundB = Math.round(b);
+  
     return roundA + roundB;
   }
 
-  if (type === 'SUBTRACT') {  
+  if (type === 'SUBTRACT') {
+    roundA = Math.round(a);
+    roundB = Math.round(b);
+  
     return roundA - roundB;
   }
 
   if (type === 'DIVIDE') {
+    roundA = Math.round(a);
+    roundB = Math.round(b);
+
     if (roundB === 0 ) {
       return ("Error");
     }
