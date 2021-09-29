a5096d90c9afb7b858992514ce90bab796691b25
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-29 16:55:04 -0500

    updating describe

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -8,19 +8,19 @@ describe('calculateNumber', function() {
     });
   });
 
-  describe('Sum integers', function() {
+  describe('Sum and round integers', function() {
     it('should be able to round and return 5', function() {
       assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
   });
 
-  describe('Sum integers', function() {
+  describe('Sum and round int to 5', function() {
     it('should be able to round and return 5', function() {
       assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
   });
 
-  describe('Sum integers', function() {
+  describe('Sum and round numbers', function() {
     it('should be able to add, round and return 6', function() {
       assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
