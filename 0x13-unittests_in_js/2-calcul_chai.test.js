75e2e0d3390bed6e52fa81dbf81803d9c293f1f4
Lilibeth Tabares <1762@holbertonschool.com>
2021-09-29 17:15:46 -0500

    updating style

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-const chai = require('chai');
+var chai = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function() {
@@ -7,25 +7,19 @@ describe('calculateNumber', function() {
       chai.expect(calculateNumber('SUM', 2, 4)).to.equal(6);
     });
   });
-});
 
-describe('calculateNumber', function() {
   describe('Subtract integers', function() {
     it('should be able to subtract and return -4', function() {
       chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
   });
-});
 
-describe('calculateNumber', function() {
   describe('Divide integers', function() {
     it('should be able to divide and return 0.2', function() {
       chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
   });
-});
 
-describe('calculateNumber', function() {
   describe('Error', function() {
     it('should be able to divide and return Error', function() {
       chai.expect(calculateNumber('DIVIDE', 3.4, 0)).to.equal('Error');
