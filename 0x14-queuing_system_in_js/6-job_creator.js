8a96ae4ccb489d4b7b194871d34abbca711e490a
Lilibeth Tabares <1762@holbertonschool.com>
2021-10-05 15:33:56 -0500

    adding message

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -4,11 +4,11 @@ const queue = kue.createQueue();
 const job = queue.create('push_notification_code', {
   phoneNumber: '4153518780'
 , message: ' This is the code to verify your account'
-}).save( function(err){
- if( !err ) console.log( `Notification job created: ${job.id}`);
+}).save( function(err) {
+ if( !err ) console.log(`Notification job created: ${job.id}`);
 });
 
-job.on('complete', () =>{
+job.on('complete', () => {
   console.log('');
 });
 
